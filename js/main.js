6f88fd5708be075d0ec004792770644db4a5043e
Tricky71 <nowwhat@mail.ru>
2023-11-30 20:52:55 +0400

    scroll button, color blocks

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
+'use strict';
 
+document.addEventListener('DOMContentLoaded', function() {
+  
+  const scrollUpBtn = document.querySelector('.longread__nav-button');
+
+  window.addEventListener('scroll', () => {
+    if(scrollY < 800) {
+      scrollUpBtn.style.display = 'none';
+    } else {
+      scrollUpBtn.style.display = 'flex';
+    }
+  });
+
+
+})
 
 
 
